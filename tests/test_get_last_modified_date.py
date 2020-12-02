12bf1e0b113b4b6c53eb6b506d5de24efb01f5d3
2224727s <2224727s@student.gla.ac.uk>
2020-12-02 12:48:00 +0000

    Added tests for github-api prototype(#39)
    Why: Tests for code coverage
    
    Problems:
    * Difficult to mock git objects​for testing
    * Internally, git objects refer to one another
    * Mocking just one object might not be enough
    
    Solutions:
    * Use git object sha-1 hash to mock objects
    
    Authors:
    * Shorya Singh

---
+++ tests/test_get_last_modified_date.py
@@ -0,0 +1,16 @@
+import pytest
+from github-api import get_last_modified_date
+from datetime import datetime
+
+class TestGetLastModifiedDate:
+    
+    def test_get_last_modified_date():
+        date_str = 'Wed, 18 Nov 2020 10:24:29 GMT'
+        assert get_last_modified_date(date_str).strftime('%a, %d %b %Y %H:%M:%S %Z')  == 'Wed, 18 Nov 2020 10:24:29 GMT'
+
+    def test_get_last_modified_date_exception():
+        date_str = 'Wed, 18 11 2020 10:24:29 GMT'
+        with pytest.raises(ValueError):
+            get_last_modified_date(date_str)
+
+

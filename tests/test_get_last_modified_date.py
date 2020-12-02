b83f1965a747033359ad12c79608d2cc1e94c42b
Ryan Murphy <2385677m@student.gla.ac.uk>
2020-12-02 14:07:08 +0000

    Fixed bug in tests (#47)
    Why: Import errors and issues with tests were causing pipeline to fail
    
    Problems:
    * Imports were failing
    * Tests weren't working as expected
    * Bug in assertion
    
    Solutions:
    * Fixed date assertion
    * Removed any API tests until we can use tokens in environment variables
    
    Authors:
    * Ryan Murphy
    * Shorya Singh

--- tests/test_get_last_modified_date.py
+++ tests/test_get_last_modified_date.py
@@ -1,16 +1,16 @@
 import pytest
-from src.github_api import get_last_modified_date
+import src.github_api as gh
 from datetime import datetime
 
 class TestGetLastModifiedDate:
     
-    def test_get_last_modified_date():
+    def test_get_last_modified_date(self):
         date_str = 'Wed, 18 Nov 2020 10:24:29 GMT'
-        assert get_last_modified_date(date_str).strftime('%a, %d %b %Y %H:%M:%S %Z')  == 'Wed, 18 Nov 2020 10:24:29 GMT'
+        assert gh.get_last_modified_date(date_str).strftime('%a, %d %b %Y %H:%M:%S')  == 'Wed, 18 Nov 2020 10:24:29'
 
-    def test_get_last_modified_date_exception():
+    def test_get_last_modified_date_exception(self):
         date_str = 'Wed, 18 11 2020 10:24:29 GMT'
         with pytest.raises(ValueError):
-            get_last_modified_date(date_str)
+            gh.get_last_modified_date(date_str)
 
 

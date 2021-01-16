ba08538b063fe03c0a02477691ccc47736088eb3
Ryan Murphy <2385677m@student.gla.ac.uk>
2021-01-16 15:54:59 +0000

    Testing pipeline

---
+++ pipeline_test.py
@@ -0,0 +1,10 @@
+def badCodeHere(code):
+    print('This is bad code')
+    return 4
+
+class this_is_Very_BadCode:
+    def __init__():
+        print('bad code')
+
+badCodeHere(1)
+x = this_is_Very_BadCode()

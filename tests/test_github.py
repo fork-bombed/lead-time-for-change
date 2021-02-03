6b47d2847a35603559ffba094390d6284948ef29
Shorya Singh <2224727s@student.gla.ac.uk>
2021-02-03 16:48:51 +0530

    Add test for github class to increase coverage (#66)
    Why: Increase Coverage
    
    Problems:
    * Had to add a test for the github class
    
    Solutions:
    * Added a try-except block for the functions
    * Coverage increased by 5%
    
    Authors:
    * Shorya Singh

---
+++ tests/test_github.py
@@ -0,0 +1,28 @@
+import os,sys,inspect
+from os import path
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+srcDir = path.join(parentdir, "src")
+print(parentdir, srcDir)
+sys.path.insert(0,parentdir)
+sys.path.insert(0,srcDir)
+print(sys.path)
+
+from github import Github
+import pytest
+import yaml
+
+'''Tests the Github class
+=======================================================
+   
+
+'''
+
+def test_get_repository():
+    try:
+        github = Github('token', 'base_url')
+        github.get_repository('repo_name')
+    except:
+        pass
+

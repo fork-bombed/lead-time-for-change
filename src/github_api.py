bb10c7eea4bbc088f5c1738da0671f32cb994471
Ryan Murphy <2385677m@student.gla.ac.uk>
2021-02-17 18:57:48 +0000

    Added template features (#73)
    Why: Templates now show difference in lead time for change from pervious releases
    
    Problems:
    * Template was too basic
    
    Solutions:
    * Added more useful information
    
    Authors:
    * Ryan Murphy

--- src/github_api.py
+++ src/github_api.py
@@ -20,12 +20,14 @@ def get_commits_between_releases(
     return commits
 
 
-def format_time(td: timedelta) -> str:
+def format_urlsafe_time(td: timedelta) -> str:
     out = []
-    hours = td.seconds//3600
-    minutes = (td.seconds//60)%60
-    if td.days > 0:
-        out.append(f'{td.days}d')
+    seconds = td.seconds
+    minutes = (seconds//60)%60
+    hours = (seconds//3600)%24
+    days = td.days
+    if days > 0:
+        out.append(f'{days}d')
     if hours > 0:
         out.append(f'{hours}h')
     if minutes > 0:
@@ -44,11 +46,23 @@ def get_lead_time(
             for c in repository.get_commits()
         ]
     else:
-        previous_release = repository.get_releases()[1]
-        commits = get_commits_between_releases(release, previous_release, repository)
+        releases = repository.get_releases()
+        release_index = None
+        for index,r in enumerate(releases):
+            if r.get_id() == release.get_id():
+                release_index = index
+                break
+        if release_index != None:
+            if release_index < len(releases)-1:
+                prev_release = releases[release_index+1]
+            else:
+                return timedelta(seconds=0)
+        else:
+            return timedelta(seconds=0)
+        commits = get_commits_between_releases(release, prev_release, repository)
         commit_times = [
-            datetime.timestamp(c.get_date()) - datetime.timestamp(previous_release.get_creation_time())
-            for c in commits    
+            datetime.timestamp(c.get_date()) - datetime.timestamp(prev_release.get_creation_time())
+            for c in commits
         ]
     return timedelta(seconds=sum(commit_times)/len(commit_times))
 
@@ -61,12 +75,29 @@ def get_release_template(
 
     with open('src/templates/default.md') as file:
         template = file.read()
+    lead_time = get_lead_time(release, repo)
+    if lead_time.days >= 30:
+        lead_time_colour = 'critical'
+    elif lead_time.days >= 10 and lead_time.days < 30:
+        lead_time_colour = 'important'
+    else:
+        lead_time_colour = 'success'
+    prev_lead_time = get_lead_time(prev_release, repo)
+    if lead_time > prev_lead_time:
+        lead_time_difference = format_urlsafe_time(lead_time - prev_lead_time)
+        lead_time_difference_colour = 'critical'
+    else:
+        lead_time_difference = ''.join(['--',format_urlsafe_time(prev_lead_time - lead_time)])
+        lead_time_difference_colour = 'success'
 
     return template.format(
         version=release.get_tag_name(),
-        lead_time=format_time(get_lead_time(release, repo)),
-        lead_time_colour='blue',
-        prev_version=prev_release.get_tag_name()
+        lead_time=format_urlsafe_time(lead_time),
+        lead_time_colour=lead_time_colour,
+        prev_version=prev_release.get_tag_name(),
+        repository=repo.get_full_name(),
+        lead_time_difference=lead_time_difference,
+        lead_time_difference_colour=lead_time_difference_colour
     )
 
 
